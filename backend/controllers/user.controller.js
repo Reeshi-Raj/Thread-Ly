31fbe275dfa76a28b03244f2743a0f47592cf704
Reeshi-Raj <reeshiraj707@gmail.com>
2026-08-08 16:03:57 +0530

    Add user profile API

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -119,3 +119,29 @@ export const getMe = async (req, res) => {
 		user: req.user,
 	});
 };
+export const getUserProfile = async (req, res) => {
+	try {
+		const { username } = req.params;
+
+		const user = await User.findOne({ username }).select("-password");
+
+		if (!user) {
+			return res.status(404).json({
+				success: false,
+				message: "User not found",
+			});
+		}
+
+		return res.status(200).json({
+			success: true,
+			user,
+		});
+	} catch (error) {
+		console.error("Get Profile Error:", error.message);
+
+		return res.status(500).json({
+			success: false,
+			message: "Internal Server Error",
+		});
+	}
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signupUser, loginUser ,logoutUser,getMe} from "../controllers/user.controller.js";
+import { signupUser, loginUser ,logoutUser,getMe,getUserProfile} from "../controllers/user.controller.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.get("/me", protectRoute, getMe);
+router.get("/profile/:username", getUserProfile);
 
 export default router;
